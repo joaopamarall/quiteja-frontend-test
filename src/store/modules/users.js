7665825f635dc2129f3a871c3a6e2e81f4ac33f5
joaopamarall <joaoamaral56@hotmail.com>
2024-12-01 18:32:08 -0300

    feat: remove funcionalidade de gerenciamento de estado

--- src/store/modules/users.js
+++ src/store/modules/users.js
@@ -1,38 +1,18 @@
-import api from "@/services/api";
-
 const state = {
   users: [],
 };
 
 
 const getters = {
-  allUsers: (state) => state.users,
+
 };
 
 const mutations = {
-  setUsers(state, users) {
-    state.users = users;
-  },
+
 };
 
 const actions = {
-  async fetchUsers({ commit }) {
-    try {
-      const { data } = await api.get("/user");
-      commit("setUsers", data.data);
-    } catch (error) {
-      console.error("Erro ao buscar usuários:", error);
-    }
-  },
-  async deleteUser({ commit, state }, userId) {
-    try {
-      await api.delete(`/user/${userId}`);
-      const updatedUsers = state.users.filter((user) => user.id !== userId);
-      commit("setUsers", updatedUsers);
-    } catch (error) {
-      console.error("Erro ao excluir usuário:", error);
-    }
-  },
+
 };
 
 export default {
